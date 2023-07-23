f7ec48895ab04e7989c0369f263d9ef713a69086
jchtwo <caspar0912+academy@googlemail.com>
2023-07-23 13:47:53 +0100

    add explanation for efficient algos

--- src/utils/multiples.ts
+++ src/utils/multiples.ts
@@ -3,6 +3,9 @@ interface iMultiplesReturn {
   multiples: number[]
 }
 
+
+// Simply loops through all numbers and use filter to return only the numbers that are a multiple.
+// I called it Brute since it involves a loop (which we can avoid)
 const bruteMultiples = (
   multiple1: number,
   multiple2: number,
@@ -17,6 +20,24 @@ const bruteMultiples = (
     multiples
   }
 }
+
+// The below works because when you can have prior knowledge of all numerical variables you can normally
+// work out a solution that invloves zero loops
+// Lets work out the mental model for this with solving the sum of a gingle multiplier's values.
+// Say our multiplier is 5 and our goal is 30
+// the numbers we are looking to sum would be 5,10,15,20 & 25 (since in the task we only take numbers
+// below the goal number)
+// we know that each of these numbers will always relate to the multiplier and be a consistent sequence
+// 5 = 1*5, 10 = 2*5, 15 = 3*5, 20 = 4*5, 25 = 5*5
+// therefore:
+// step1 => we devide the goal (-1 so that we don't include the goal) by the multiplier.
+// This will always be the number of numbers we will need to sum
+// step2 =>  we can algebraically reduce the sum logic.
+// (5,10,15,20,25)
+// is the same as (1x5, 2x5, 3x5, 4x5, 5x5)
+// is the same as 5 x (1+2+3+4+5)
+// (1+2+3+4+5) is the same as 5 * the median value of (1,2,3,4,5)
+// so we end up with 5 * 5 * ((1+ 5) / 2)
 
 const sumOneMultiple = (multiple: number, goal: number): number => {
   const timesMultiplied = Math.floor((goal - 1) / multiple)
--- src/utils/primes.ts
+++ src/utils/primes.ts
@@ -56,6 +56,23 @@ const semiBruteIsPrime = (num:number): boolean => {
     return true
 }
 
+// This is more efficient than the above. We are essentially doing the same thing, just with less loops.
+// step1 => We create an array of 'true' values.
+// step2 => we create an empty array (this will store the primes)
+// step3 => we set an upper limit for the numbers we will need to validate (as multiples)
+// This works because any number after a numbers square root cannot be a new factor
+// of the number (because it would end up greater than the number!!)
+// step4 => we create a loop of all the potential prime factors, 2 to the square root of our max
+// step5 => we find the first potential prime factor (always 2)
+// step6 => we create another loop running from our number (e.g 2), until our max value.
+// The loop is incremented so that it will check every multiple of that number and mark it 'false'
+// which in 2's case would be (2,4,6,8....etc).
+// This repeats until we hit the square root of our max. At this point we should have found every
+// potential prime!!
+// step7 => we then loop through the seived array and push any values that remain 'true' into our
+// primes array since these will be the primes
+// step8 => we use filter to remove numbers below the min value if this has been set and reduce to
+// sum all the primes, these are then returned.
 const seivePrimes = (min: number, max: number): iPrimesReturn => {
     const seiveArray = Array.from({ length: max }, () => true)
     const primesArray = []
